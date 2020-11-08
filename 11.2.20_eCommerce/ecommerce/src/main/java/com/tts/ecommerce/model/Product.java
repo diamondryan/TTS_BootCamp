b24f5f2786d9f77f386cc272b0a3b39d280cdad1
Diamond Ryan <diamondschilds@gmail.com>
2020-11-08 01:09:37 -0500

    created product page

---
+++ 11.2.20_eCommerce/ecommerce/src/main/java/com/tts/ecommerce/model/Product.java
@@ -0,0 +1,102 @@
+package com.tts.ecommerce.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="product_id")
+    private Long id;
+
+    private int quantity;
+    private float price;
+    private String description;
+    private String name;
+    private String brand;
+    private String category;
+    private String imgURL;
+
+    // public Product() {
+    // }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [brand=" + brand + ", category=" + category + ", description=" + description + ", id=" + id
+                + ", imgURL=" + imgURL + ", name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+    }
+
+    
+    
+
+
+}
