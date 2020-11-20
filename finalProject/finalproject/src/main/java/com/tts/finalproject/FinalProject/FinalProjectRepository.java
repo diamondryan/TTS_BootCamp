d0b9fb372d8e964d0573f7400505b57ce7b791dc
Diamond Ryan <diamondschilds@gmail.com>
2020-11-20 15:53:09 -0500

    deleted

--- finalProject/finalproject/src/main/java/com/tts/finalproject/FinalProject/FinalProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tts.finalproject.FinalProject;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FinalProjectRepository extends CrudRepository<FinalProject, Long>{
-    
-}
